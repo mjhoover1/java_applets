b7cafa93e3cb471f5566c6d4dc797f0de880bf7c
mjhoover1 <mjhoover@Montanas-Dell>
2023-11-16 13:13:35 -0500

    Got Makefile working and created jar files

--- vasco/common/CompileDate.java
+++ vasco/common/CompileDate.java
@@ -1,5 +1,5 @@
 package vasco.common;
 
 public class CompileDate {
-   public final static String compileDate = "Compiled on Jun 07, 2022";
+   public final static String compileDate = "Compiled on Nov 16, 2023";
 }
