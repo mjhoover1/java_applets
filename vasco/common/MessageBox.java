494b75631b093c0e4dc2da771f73787cce5eebdc
mjhoover1 <mjhoover@Montanas-Dell>
2023-11-28 14:13:30 -0500

    Added conversion from Awt to Swing for MessageBox

--- vasco/common/MessageBox.java
+++ vasco/common/MessageBox.java
@@ -5,6 +5,11 @@ import javax.swing.*; // import java.awt.*;
 import javax.swing.event.*; // import java.awt.event.*;
 // import java.applet.*;
 import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.*;
 
 public class MessageBox extends JDialog implements ActionListener {
@@ -22,7 +27,7 @@ public class MessageBox extends JDialog implements ActionListener {
     setLocation(( screen.width - getSize().width ) / 2,
 	 ( screen.height - getSize().height ) / 2 );
     setResizable(false);
-    show();
+    setVisible(true); // show();
   }
 
     public void actionPerformed(ActionEvent event) {
